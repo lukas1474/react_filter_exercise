e6c1b2722ae8997fd7b9beb48ca3f3c8550ceb53
Lukasz Sobel <lukasz.sobel@hotmail.com>
2021-06-09 19:24:53 +0200

    add render people list

--- src/people/People.jsx
+++ src/people/People.jsx
@@ -7,22 +7,28 @@ import { bindActionCreators } from 'redux';
 
 class People extends Component {
   static propTypes = {
-    // ...
+    people: PropTypes.array,
   };
 
-  // ...
-
+  renderList = () => {
+    return this.props.people.map(human => (
+       <div className='App-box' key={human.id}>
+        {human.name}
+      </div>
+    ))
+  }
+  
   render() {
     return (
       <div>
-        {/* ... */}
+        {this.renderList()}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  // ...
+  people: state.people.people,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
